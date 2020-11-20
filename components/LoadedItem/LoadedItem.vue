1ed7529cbf6a4a9f0970e51ac7d3b159eb7e7321
Dmitry N <dmitrynichi@gmail.com>
2020-11-20 21:41:59 +0300

    feat(loading): loaded item component

---
+++ components/LoadedItem/LoadedItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <div class="pic" :style="`background: url(${src})`"></div>
+    <div class="metadata">
+      <div class="title">{{ title }}</div>
+      <div class="loading-time">
+        {{ loadingTime.toISOString().replace('T', ' ').slice(0, 19) }}
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    loadingTime: {
+      type: (Date as any) as () => Date,
+      required: true,
+    },
+  },
+})
+</script>
+<style scoped>
+.container {
+  display: flex;
+}
+.pic {
+  width: 169px;
+  height: 88px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.metadata {
+  margin-left: 27px;
+}
+.title {
+  font: normal normal bold 22px/25px Roboto, sans-serif;
+  letter-spacing: 0;
+  color: #707070;
+  opacity: 1;
+}
+.loading-time {
+  margin-top: 5px;
+  font: normal normal normal 22px/25px Roboto, sans-serif;
+  letter-spacing: 0;
+  color: #707070;
+  opacity: 1;
+}
+</style>
---
+++ components/LoadedItem/index.ts
@@ -0,0 +1,1 @@
+export { default as LoadedItem } from './LoadedItem.vue'
