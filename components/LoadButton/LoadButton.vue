67a14e90e3748d343af07bd3d570a5bca9369caa
Dmitry N <dmitrynichi@gmail.com>
2020-11-20 20:59:58 +0300

    feat(main-page): load button

---
+++ components/LoadButton/LoadButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button type="button" @click="onClick">Загрузить</button>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  methods: {
+    onClick() {
+      console.log('clicked')
+    },
+  },
+})
+</script>
+<style scoped>
+button {
+  background: #006896 0% 0% no-repeat padding-box;
+  box-shadow: 0 2px 2px #0000003d;
+  border-radius: 2px;
+  opacity: 1;
+  font: normal normal normal 16px/19px Roboto, sans-serif;
+  letter-spacing: 0;
+  color: #fff;
+  width: 168px;
+  height: 36px;
+  border: 0;
+}
+</style>
---
+++ components/LoadButton/index.ts
@@ -0,0 +1,1 @@
+export { default as LoadButton } from './LoadButton.vue'
